422be2656ffbdf7575e2002578135788b7cecfc2
Julien Desrosiers <juliend2@gmail.com>
2024-09-17 21:39:27 -0400

    Début d'une navigation

--- js/src/app.js
+++ js/src/app.js
@@ -5,15 +5,16 @@ export default class App {
     constructor(container, chapelets) {
         this.container = container;
         this.chapelets = chapelets;
+        this.currentChapelet = this.chapelets[3]
 
-        this.init(this.chapelets[3]);
+        this.init(this.currentChapelet);
     }
 
     init(chapelet) {
         const html = this.sectionsHTML(chapelet)
         document.querySelector(this.container).innerHTML = html;
 
-        new fullpage('#fullpage', {
+        new fullpage('#fullpage #slides', {
             sectionsColor: chapelet.backgroundColors,
             anchors: chapelet.anchors,
             // TODO: get a proper license and revert my hacks to disable license checking.
@@ -24,10 +25,17 @@ export default class App {
     }
     
     sectionsHTML(chapelet) {
-        return chapelet.mysteres.map(mystere => {
-            return `<div class="section">${
-                mystere.toHTML()
-            }</div>`
-        }).join('')
+        return `<nav id="navigation">
+                ${chapelet.name}
+            </nav>
+            <section id="slides">
+            ${
+                chapelet.mysteres.map(mystere => {
+                    return `<div class="section">${
+                        mystere.toHTML()
+                    }</div>`
+                }).join('')
+            }
+            </section>`
     }
 }
